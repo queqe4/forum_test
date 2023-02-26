d071bbfbe164738e1b102670acb16f9eccd2d69d
queqe4 <queqe4566@gmail.com>
2023-02-27 00:58:52 +0300

    initial Commit

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'nephilim6466',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '6664',
     }
